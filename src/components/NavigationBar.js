d7024a21e5d289ba519fb285ba58ece7075b779b
Calvin <calvin@Megans-MacBook-Pro-3.local>
2022-07-26 22:02:35 -1000

    Cognito Logins added

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,8 +1,9 @@
 
 
 import React from "react";
-import {Navbar, Nav} from "react-bootstrap";
+import {Navbar, Nav, Button} from "react-bootstrap";
 import styled from 'styled-components'
+
 
 
 
@@ -14,8 +15,59 @@ const StyledNav = styled(Navbar)`
   border-radius: 2px;
 `;
 
-class NavB extends React.Component{
+
+
+const login_url = 'https://spliticist.auth.us-west-1.amazoncognito.com/login?client_id=16kr7l3gd2uhro89pfnkk77api&response_type=token&scope=email+openid&redirect_uri=https://spliticist.com';
+// const login_url = 'http://localhost:3000?client_id=16kr7l3gd2uhro89pfnkk77api&response_type=token&scope=email+openid&redirect_uri=https://spliticist.com';
+      //  console.log("loggin in!")
+      //  const curUrl = window.location.href;
+
+
+      class NavB extends React.Component{
+        constructor(props){
+          super(props)
+          this.LoginButton= (<Button style = {{"position": "absolute", "right": "3rem"}} onClick = {this.onclickfunc} href = {login_url}>Login</Button>)    
+          this.LogoutButton= (<Button style = {{"position": "absolute", "right": "3rem"}} onClick = {this.onclickfunc} href = "https://spliticist.com">Logout</Button>)
+          
+          this.state = {
+            activeButton: this.LoginButton        
+      }
+    this.onclickfunc=this.onclickfunc.bind(this)
+
+    }
+    componentDidMount(){
+      if (localStorage.getItem("id_token")===null){
+        const curUrl = window.location.href;
+        if (curUrl.includes("id_token")){
+          console.log("setting local storage")
+          const id_token = (curUrl.split("#id_token=")[1]).split("&")[0]
+          const access_token = (curUrl.split("#id_token=")[1]).split("&")[1].split("access_token=")[1]
+          localStorage.setItem("id_token", id_token)
+          localStorage.setItem("access_token", access_token)
+        } 
+      } 
+    if (localStorage.getItem("id_token")===null){
+      console.log("setting login button")
+      this.setState({activeButton: this.LoginButton})
+    } else {
+      console.log("setting logout button")
+      this.setState({activeButton: this.LogoutButton})
+    }
+    }
+
+    onclickfunc(){
+      localStorage.clear()
+      console.log(this.state.activeButton)
+      if (this.state.activeButton==this.LoginButton){
+        this.setState({activeButton: this.LogoutButton})
+      } else {
+        this.setState({activeButton: this.LoginButton})
+      }
+
+    
+    }
     render(){
+
         return (
             <StyledNav bg="light" expand="lg">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -24,6 +76,8 @@ class NavB extends React.Component{
                 <Nav.Link href="/">My Split</Nav.Link>
                 <Nav.Link href="/Edit">Edit</Nav.Link>
                 <Nav.Link href="/Contact">Contact</Nav.Link>
+
+                {this.state.activeButton}
               </Nav>
             </Navbar.Collapse>
         </StyledNav>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,57 @@
 import React from 'react';
 import styled from 'styled-components';
+import AWS from 'aws-sdk'
 
 const StyledHomePage = styled.div`
   color: blue
 `;
+// const login_url = 'https://spliticist.auth.us-west-1.amazoncognito.com/login?client_id=16kr7l3gd2uhro89pfnkk77api&response_type=token&scope=email+openid&redirect_uri=https://spliticist.com';
+// const login_url = 'http://localhost:3000?client_id=16kr7l3gd2uhro89pfnkk77api&response_type=token&scope=email+openid&redirect_uri=https://spliticist.com';
+
+
 
 class HomePage extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            loggedIn: false
+        }
     }
+    componentDidMount(){
+        console.log(localStorage)
+        if (localStorage.getItem("id_token")===null){
+            this.setState({loggedIn: false})
+        } else {
+            this.setState({loggedIn: true})
+        }
+    }
+    
     render(){
-        return(
+        if (this.state.loggedIn === false){
+            
 
-                <StyledHomePage>Home Page</StyledHomePage>
-        )
+            return(<StyledHomePage>You have NOT logged in</StyledHomePage>)
+        } else {
+            const id_token = localStorage.getItem("id_token")
+            const access_token = localStorage.getItem("access_token")
+            AWS.config.region = 'us-west-1'; // Region
+            AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                IdentityPoolId: 'us-west-1:3e1e5c4a-c1ae-44e0-a72b-c4ec1b3a791c',
+                Logins: {
+                    'cognito-idp.us-west-1.amazonaws.com/us-west-1_jrfjOTZfg': id_token
+                }                
+            }); 
+            const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();  
+            // const params = {
+            //     AccessToken: access_token /* required */
+            //   };
+            cognitoidentityserviceprovider.getUser({AccessToken: access_token}, function(err, data) {
+                if (err) console.log(err, err.stack); // an error occurred
+                else     console.log(data);           // successful response
+              });                     
+            return(<StyledHomePage>You have logged in</StyledHomePage>)
+        }
+    
     }
 }
 
